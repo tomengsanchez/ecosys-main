f17b34b6aed4ce4aef46fc4554a84765cc8d8526
michael sanchez <mrtomy31@gmai.com>
2025-06-03 09:42:10 +0800

    maindb2

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
  */
 
 // ** MySQL settings ** //
-define('DB_NAME', 'mainsystem');
+define('DB_NAME', 'maindb2');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
